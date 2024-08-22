b86faf765ad8084096f629d4bfbdc76ac68dd317
Mileidy Vanegas Martínez <vanegasmartinezmileidy@gmail.com>
2024-08-22 13:24:42 -0500

    Create Class TodoBook and Constructor

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -18,4 +18,11 @@ class Todo:
         return f"{self.code_id} - {self.title}"
 
 
+
+class TodoBook:
+
+    def __init__(self, todos: dict[int, Todo] = {}):
+        self.todos = todos
+
+    
     
